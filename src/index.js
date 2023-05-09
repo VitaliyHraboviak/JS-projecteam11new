48386e25a8cae6573e3657954700720048fc6fbc
IgorKryvenko56 <ikryvenko@yahoo.co.uk>
2023-05-09 12:56:46 +0300

    JS-projecteam11new import added

--- src/index.js
+++ src/index.js
@@ -4,3 +4,8 @@ import './js/best_sellers_render';
 import './js/best_sellers_scroll_to_top';
 import './js/category_chosen';
 import './js/category_book_profile';
+import './js/category';
+import './js/change_theme';
+import './js/modal_window_object';
+import './js/shopping_list';
+import './js/support_Ukraine';
