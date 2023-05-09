1609cc8caf05da9dc35597376ed7330aa0f75767
IgorKryvenko56 <ikryvenko@yahoo.co.uk>
2023-05-09 12:22:40 +0300

    JS-projecteam11new import updated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './js/modal_window_close';
-import './js/best_sellers.js';
-import './js/best_sellers_scroll_to_top.js';
-import './js/category_chosen.js';
-import './js/category_book_profile.js';
+import './js/best_sellers_fetch';
+import './js/best_sellers_render';
+import './js/best_sellers_scroll_to_top';
+import './js/category_chosen';
+import './js/category_book_profile';
