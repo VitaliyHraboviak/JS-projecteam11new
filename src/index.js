2d207d14c6d1bf6e7dd5675c00223a294f046f8d
IgorKryvenko56 <ikryvenko@yahoo.co.uk>
2023-05-12 00:33:40 +0300

    JS-projecteam11new

--- src/index.js
+++ src/index.js
@@ -10,3 +10,4 @@ import './js/modal_window_object';
 // import './js/shopping_list';
 import './js/athorization_modal_user';
 import './js/authorization_modal_close';
+import './js/support_Ukraine';
